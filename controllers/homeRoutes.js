cdd78e290546ce6f096302afc5bd23b05c89e3c3
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-06-24 15:27:31 -0700

    added separate dashboard routes file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        //get all posts
+    }
+})
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 //requiring the api and home routes
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashRoutes = requir('./dashboardRoutes')
 
 //use / for homeRoutes and /api for apiRoutes files
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashRoutes)
 
 module.exports = router;
