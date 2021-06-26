f9dd28ec54b0a86a3bbd20ece0eef6279fd008dc
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-06-26 15:18:45 -0700

    added routes for posts and comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -17,4 +17,39 @@ router.post('/', async (req,res) => {
     }
 });
 
+router.put('/:id', async (req,res) => {
+    try {
+        const newPost = await Post.update({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.body.id,
+        // user_id: req.session.user_id,
+        },
+        {
+            where: {
+                id: req.params.id,
+            }
+        });
+        console.log(newPost)        
+        res.status(200).json(newPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.delete('/:id', async (req,res) => {
+    try {
+        const newPost = await Post.destroy(
+        {
+            where: {
+                id: req.params.id,
+            }
+        });
+        console.log(newPost)        
+        res.status(200).json(newPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 module.exports = router;
